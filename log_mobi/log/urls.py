b7007f71024ed2a78c7a41c4a539b8d19e882628
adsons3c <adsonemanuels3c@gmail.com>
2018-11-27 14:15:24 -0300

    Correção de urls

--- log_mobi/log/urls.py
+++ log_mobi/log/urls.py
@@ -5,5 +5,5 @@ app_name="log"
 urlpatterns = [
     path('registro_pagamento', Autolist.as_view()),
     path('envio_notificacao', Envio_notificacao.as_view()),
-    path('registro_detalhes/<int:pk>', AutoDetail.as_view(), name='registro_detalhes'),
+    path('registro_detalhes/<int:pk>/', AutoDetail.as_view(), name='registro_detalhes'),
 ]
